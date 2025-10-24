70fd4a5989b07cb7c33da05d2f8a9bbf5ecd213d
Alessio Bianchetti <alessio.bianchetti@gmail.com>
2025-10-24 23:57:42 +0200

    chore(home): remove NOSONAR comment from meta function

--- apps/web/app/routes/home.tsx
+++ apps/web/app/routes/home.tsx
@@ -2,7 +2,6 @@ import type { Route } from './+types/home';
 import { Welcome } from '../welcome/welcome';
 
 export function meta({}: Route.MetaArgs) {
-  // NOSONAR
   return [
     { title: 'New React Router App' },
     { name: 'description', content: 'Welcome to React Router!' },
