84057159732ad99e9e8ccb6c0067d0fb7b6ae660
Alessio Bianchetti <alessio.bianchetti@gmail.com>
2025-11-01 20:44:48 +0100

    refactor(eslint): add tsconfigRootDir to parserOptions for TypeScript support

--- frontend/web/eslint.config.js
+++ frontend/web/eslint.config.js
@@ -1,10 +1,21 @@
 import js from '@eslint/js';
-import tseslint from 'typescript-eslint';
 import prettierRecommended from 'eslint-plugin-prettier/recommended';
+import tseslint from 'typescript-eslint';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const tsconfigRootDir = path.dirname(fileURLToPath(import.meta.url));
 
 export default [
   {
     ignores: ['**/dist/**', '**/node_modules/**', '**/build/**', '**/.react-router/**'],
+  },
+  {
+    languageOptions: {
+      parserOptions: {
+        tsconfigRootDir,
+      },
+    },
   },
   js.configs.recommended,
   ...tseslint.configs.recommended,
